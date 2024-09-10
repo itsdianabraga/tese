f35e237897df72ed1cfc403c3ab37c3c5329efd8
itsdianabraga <92678962+itsdianabraga@users.noreply.github.com>
2024-09-10 15:29:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ def run_script(script_name):
     """Run a script using subprocess."""
     result = subprocess.run(['python', script_name], capture_output=True, text=True)
     print(f"Running {script_name}...")
-    print("Output:", result.stdout)
     if result.stderr:
         print("Errors:", result.stderr)
     result.check_returncode()
@@ -12,11 +11,13 @@ def run_script(script_name):
 def main():
     try:
         # Run the scripts in order
-        run_script('tweets_extraction.py')
-        run_script('pre-processing.py')
-        run_script('processing.py')
-        run_script('folder_statistics.py')  # Assuming this is a script for statistics
-        run_script('mongo_db.py')  # Assuming this is the script interacting with MongoDB
+        run_script('./tweets_data/tweets_extraction.py')
+        run_script('./data_clean/pre-processing.py')
+        run_script('./data_clean/processing.py')
+        run_script('./statistics/each_category.py')
+        run_script('./statistics/global.py')
+        run_script('./statistics/links.py')
+        run_script('./mongo_conection/mongo_db.py')  # Assuming this is the script interacting with MongoDB
 
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running the scripts: {e}")
